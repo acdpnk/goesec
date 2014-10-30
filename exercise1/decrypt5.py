449467ed3d2d5b1cf8a624b8a24addd99807372d
chrifpa <chrifpa@gmail.com>
2014-10-30 19:59:47 +0100

    snapshot

--- exercise1/decrypt5.py
+++ exercise1/decrypt5.py
@@ -1,4 +1,6 @@
 from string import ascii_lowercase
+from itertools import product
+from functools import reduce
 
 crypt_path = "crypt5.txt"
 crypt = ""
@@ -34,7 +36,7 @@ def decrypt(key, ciphertext):
     return out
 
 def plausibility_check(message_candidate):
-    # computes a relative score for the likelihood of message_candidate beeing
+    # computes a relative score for the likelihood of message_candidate being
     # a text in the english language (that is, the correct decrypted message)
     score = 0
     with open('/usr/share/dict/words') as dictionary:
@@ -43,19 +45,55 @@ def plausibility_check(message_candidate):
                 # if message_candidate contains the word once or more
                 # print(word.strip())
                 score += 1
-    print(score)
+    # print(score)
     return score
 
 def guess_key(keylen, depth, ciphertext):
 
     # first, we create a symbol map: a list of symbols for each part of the key
-    # that symbol was encrypted with, sorted by frequency
+    # that symbol was encrypted with, and from that a frequency map: the
+    # ascii letters sorted by frequency in their respective lists
     symbol_map = ['' for i in range(keylen)]
+    frequency_map = ['' for i in range(keylen)]
     for i in range(len(ciphertext)):
         symbol_map[i % keylen] += ciphertext[i]
     for i in range(len(symbol_map)):
-        symbol_map[i] = sorted(ascii_lowercase, key=lambda symbol: symbol_map[i].find(symbol), reverse=True)
+        frequency_map[i] = sorted(ascii_lowercase, key=lambda symbol: symbol_map[i].find(symbol), reverse=True)
 
+    # lowercase ascii letters, sorted by relative frequency in the english
+    # language. See https://en.wikipedia.org/wiki/Letter_frequency
     frequent_letters = 'etaonrishdlfcmugypwbvkjxqz'
 
+    # generate the list of permutations used to iterate over the key candidates
+    # there has to be a better way to do this - if you know it, please tell me
+    # the indexlist is generated as follows:
+    # 1) we take the nth cartesian product of range(depth), where n = keylen
+    #    that means for our key candidates we take the [depth] most frequent
+    #    letters from each list
+    # 2) we sort the resulting list of tuples be the tuple's digit sum
+    # when we use the resulting list to generate our list of key candidates,
+    # they will be ordered by likelihood of them being the correct key
+    # actually, this is probably not needed. what the fuck.
+    indexlist = list(product(range(depth), repeat=keylen))
+    # indexlist.sort(key = lambda x: reduce(lambda y,z: y+z, x))
+    # commented out because probably useless
+
+    
+
     print(symbol_map)
+    print(frequency_map)
+
+
+# max_score = 0
+# message = ""
+# for key_candidate in product('etao', repeat=5):
+#     key_candidate = ''.join(key_candidate)
+#     print(key_candidate)
+#     message_candidate = decrypt(key_candidate, crypt)
+#     print(message_candidate)
+#     score = plausibility_check(message_candidate)
+#     if score > max_score:
+#         message = message_candidate
+#         max_score = score
+#
+# print(message)
