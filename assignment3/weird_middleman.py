97ef937aa877b80a115a21821522ca3b94db057b
chrifpa <chrifpa@gmail.com>
2014-11-14 14:23:31 +0100

    decode bytestrings to utf-8

--- assignment3/weird_middleman.py
+++ assignment3/weird_middleman.py
@@ -3,6 +3,7 @@
 import socket
 from ssl import SSLSocket
 from argparse import ArgumentParser
+from sys import exit
 
 parser = ArgumentParser(description='Middleman attack on weird.goesec.de:9999',
                         epilog='EXAMPLE: $ python3 %(prog)s -v -o weird_capture.txt')
@@ -20,6 +21,7 @@ try:
     sock2.connect(("weird.goesec.de", 9999))
 except:
     print("failed to establish connection")
+    exit(1)
 
 
 capture = '' # collect captured messages here
@@ -32,14 +34,14 @@ while sock1 and sock2:
             s1_capture = sock1.recv()
             if not s1_capture: break
             sock2.send(s1_capture)
-            if args.verbose: print("sock1: %s" % s1_capture)
-            capture = "%s%s\n" % (capture, s1_capture)
+            if args.verbose: print("sock1: %s" % s1_capture.decode("utf-8"))
+            capture = "%s%s" % (capture, s1_capture.decode("utf-8"))
         if sock2:
             s2_capture = sock2.recv()
             if not s2_capture: break
             sock1.send(s2_capture)
-            if args.verbose: print("sock2: %s" % s2_capture)
-            capture = "%s%s\n" % (capture, s2_capture)
+            if args.verbose: print("sock2: %s" % s2_capture.decode("utf-8"))
+            capture = "%s%s" % (capture, s2_capture.decode("utf-8"))
     except:
         print("connection dropped")
         break
